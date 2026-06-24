1e35c96b0d91cbbe16b28f99d55cc5b6c6ea6262
mangeris4thepeople <mangeris4thepeople2026@gmail.com>
2026-06-23 22:55:37 -0600

    Delete src/App.jsx

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-<div className="home-stat__div" />
-            <div className="home-stat">
-              <span className="home-stat__num">32</span>
-              <span className="home-stat__label">CSI Divisions</span>
-            </div>
